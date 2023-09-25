e31b703b8b1ab290946882f0372741d95f35aa86
박은성 <cro3179@naver.com>
2023-09-25 18:16:41 +0900

    Info3 페이지 추가 /필터수정 / 로딩페이지 만드는중 / 페이지네이션 수정

--- src/components/Info_Test.js
+++ src/components/Info_Test.js
@@ -7,7 +7,7 @@ function Info_Test() {
   //  페이지 출력
   const list = 10;
   //페이지
-  // 같은 api주소에서 토탈카운트 뽑기
+  // 같은 api주소에서 토탈카운트 뽑기https://github.com/dldmswn0719/Team-Puri
   let [page, setPage] = useState(1)
   // 페이지 5개씩
   const pagination = 5;
@@ -52,8 +52,9 @@ function Info_Test() {
   }
 
   // 지역 도시 city
+  const [cityCode , setCityCode] = useState("")
   const [city, setCity] = useState([])
-  
+
   useEffect(() => {
 
     fetch(`https://apis.data.go.kr/1543061/abandonmentPublicSrvc/sido?numOfRows=100&pageNo=1&serviceKey=${process.env.REACT_APP_apiKey}&_type=json`).then((res) => {
@@ -86,18 +87,18 @@ function Info_Test() {
 
   // 기타동물
   const [etc, setEtc] = useState([])
-  useEffect(() => {
-    async function getCatData() {
-      const response = fetch(`http://apis.data.go.kr/1543061/abandonmentPublicSrvc/kind?up_kind_cd=429900&serviceKey=${process.env.REACT_APP_apiKey}&_type=json`);
-      return response;
-    }
-    getCatData()
-      .then(res => res.json())
-      .then(json => setEtc(json.response.body.items.item));
-  }, []);
+  //  useEffect(() => {
+  //    async function getCatData() {
+  //      const response = fetch(`http://apis.data.go.kr/1543061/abandonmentPublicSrvc/kind?up_kind_cd=429900&serviceKey=${process.env.REACT_APP_apiKey}&_type=json`);
+  //      return response;
+  //    }
+  //    getCatData()
+  //      .then(res => res.json())
+  //      .then(json => setEtc(json.response.body.items.item));
+  //  }, []);
 
   //축종
-  const [kind, setKind] = useState(["모든 축종", "개", "고양이","기타"])
+  const [kind, setKind] = useState(["모든 축종", "개", "고양이", "기타"])
 
 
   // http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20211201&endde=20211231&pageNo=1&numOfRows=10&serviceKey=인증키(URL Encode)
@@ -105,23 +106,24 @@ function Info_Test() {
 
   const [selected, setSelected] = useState("모든 축종", "기타");
   const [selectedSec, setSelectedSec] = useState("모든 품종");
-
   const [selectData, setSelectData] = useState()
-
+  
+  const [kindCode , setKindCode] = useState("")
 
   function FilterData(data) {
 
     let value = [];
     if (data === '개') {
       value = dog;
-    } else if(data ==='고양이'){
+    } else if (data === '고양이') {
       value = cat;
-    }else if(data === '기타'){
+    } else if (data === '기타') {
       value = etc;
     }
     return setSelectData(value);
 
   }
+  const [kindSelect, setKindSelect] = useState("");
 
   // 모든 유기동물 데이터
   const [data, setData] = useState([])
@@ -129,7 +131,7 @@ function Info_Test() {
   const [defaultData, serDefaultData] = useState(data);
   // console.log(defaultData)
   useEffect(() => {
-    fetch(`https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20211201&endde=20211231&pageNo=${page}&numOfRows=100&serviceKey=${process.env.REACT_APP_apiKey}&_type=json`).then((res) => {
+    fetch(`https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20211201&endde=20211231&upr_cd=${cityCode}&pageNo=${page}&kind=${kindSelect}&numOfRows=12&serviceKey=${process.env.REACT_APP_apiKey}&_type=json&up_kind_cd=${kindCode}`).then((res) => {
       return res.json()
     }).then(async (data) => {
       const result = await data.response.body.items.item;
@@ -140,47 +142,63 @@ function Info_Test() {
       setTotalCnt(resultCnt)
     });
 
-  }, [page]);
+  }, [page,cityCode, kindCode, kindSelect]);
 
+
+// https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?bgnde=20211201&endde=20211231&upr_cd=6110000&up_kind_cd=417000&kind=000054&pageNo=1&numOfRows=1000&serviceKey=5tteN0Ji1rrEE90jMPQFev%2BtioKVvE76RHyN9%2Bji7h%2B9AspJWcBKdXX6CpccJfgxKYmAmVYjQ6TbimU37dQQcw%3D%3D&_type=json
+  
 
   const [optionCitySelect, setOptionCitySelect] = useState("");
   const [subOptionCitySelect, setSubOptionCitySelect] = useState("");
   const [optionKindSelect, setOptionKindSelect] = useState("");
   const [isChoice, setIsChoice] = useState([0, 0, 0])
   const copyChoice = [...isChoice]
-// console.log(copyChoice)
+  // console.log(copyChoice)
 
 
   const optionCity = (e) => {
-    const cityValue = e.target.value
-    setOptionCitySelect(cityValue);
-    // let first = copyChoice[0]
-    let firstOption = [...isChoice]
-    // console.log(third)
-    if (cityValue !== '모든 지역') {
-      firstOption[0] = 1
-      setIsChoice(firstOption)
-    }else{
-      firstOption[0] = 0
-      setIsChoice(firstOption)
-    }
+    setCityCode(e.target.value)
+    // const cityValue = e.target.value
+    // setCity(cityValue)
+    // setOptionCitySelect(cityValue);
+    // // let first = copyChoice[0]
+    // let firstOption = [...isChoice]
+    // // console.log(third)
+    // if (cityValue !== '모든 지역') {
+    //   firstOption[0] = 1
+    //   setIsChoice(firstOption)
+    // } else {
+    //   firstOption[0] = 0
+    //   setIsChoice(firstOption)
+    // }
   }
 
 
-  const subOption = (e)=>{
-        //축종의 value값으로 품종 변환
-        setSelected(e.target.value);
-        //품종의 value값 출력
-        FilterData(e.target.value)
+
+  const subOption = (e) => {
+    //축종의 value값으로 품종 변환
+    const Array = ["","417000", "422400", "429900"]
+    // alert (e.target.value)
+    setKindCode(Array[e.target.value])
+
+    // setSelected(e.target.value);
+    //품종의 value값 출력
+    // FilterData(e.target.value)
     const subValue = e.target.value
     setSubOptionCitySelect(subValue)
     let secondOption = [...isChoice]
+    setOptionKindSelect("모든 품종");
+
+
     // console.log(third)
     if (subValue !== '모든 축종') {
       secondOption[1] = 1
       setIsChoice(secondOption)
-     }else{
+
+    } else {
       secondOption[1] = 0
+
+
       setIsChoice(secondOption)
     }
   }
@@ -189,13 +207,14 @@ function Info_Test() {
 
   const optionKind = (e) => {
     const KindValue = e.target.value
+    setKindSelect(KindValue)
     setOptionKindSelect(KindValue);
     let thirdOption = [...isChoice]
     // console.log(third)
     if (KindValue !== '모든 품종') {
       thirdOption[2] = 1
       setIsChoice(thirdOption)
-    }else{
+    } else {
       thirdOption[2] = 0
       setIsChoice(thirdOption)
     }
@@ -205,9 +224,6 @@ function Info_Test() {
 
 
     const FilterAll = data && data.filter((e, i) => {
-
-     
-      //   const cityOneremove = e.orgNm.replace("특별" , "");
       const cityValueArray = e.orgNm.split(" ")[0] === optionCitySelect
       //  console.log("cityValueArray 도시" ,cityValueArray )
       // 축종 거르는것
@@ -215,45 +231,27 @@ function Info_Test() {
       // console.log("subValueArray 축종", subValueArray)
       const KindValueArray = e.kindCd.includes(optionKindSelect);
       //  console.log("KindValueArray 품종", KindValueArray)
-
-
-
-      const etcRemove = !e.kindCd.includes("[기타]");
-
-      // const CntKind =  data.filter(el => el.kindCd.includes(optionKindSelect)).length;
-
       //모든 지역 모든 축종 모든 품종 ㅇ
       if (isChoice[0] === 0 && isChoice[1] === 0 && isChoice[2] === 0) {
         return e
-        
-        // 모든 지역이 아니고 모든 축종 모든 품종 
-      }else if(isChoice[0] === 1 && isChoice[1] === 0 && isChoice[2] === 0){
-        return cityValueArray 
-        
+        //  지역선택 모든 축종 모든 품종 ㅇ
+      } else if (cityValueArray && isChoice[1] === 0 && isChoice[2] === 0) {
+        return cityValueArray
       }
-      else if(isChoice[0] ===  1 && subValueArray  && isChoice[2]=== 1)
-      {
-        // 모든 지역이 아니고 축종이 바뀌고 품종이 바뀔때
-        return  cityValueArray& subValueArray && KindValueArray
+      // 지역이 바뀌고  축종이 바뀌고 품종이 품종이 바뀔때
+      else if (cityValueArray && subValueArray && KindValueArray) {
+        return cityValueArray && subValueArray && KindValueArray
+        // 지역선택 -  축종 선택 -  모든 품종일때
+      } else if (cityValueArray && subValueArray && isChoice[2] === 0) {
+        return subValueArray
+        // 모든지역 축종이 바뀌고 모든 품종일때 
+      } else if (isChoice[0] === 0 && subValueArray && isChoice[2] === 0) {
+        return subValueArray
+        //  모든지역 축종바뀌고 품종바뀔때  
+      } else if (isChoice[0] === 0 && subValueArray && KindValueArray) {
+        return subValueArray && KindValueArray
       }
-      // 축종만 바뀔때 
-      // else if(idsChoise[0] === 0 ){
-      //   return
-      // }
-      // // else if(isChoice[0]===1 && subValueArray && isChoice[2]===0){
-      //   return subValueArray
-      // }
-    
-      // 모든 지역이고 축종이 바뀌고 품종이 바뀔때 
-      // else if(isChoice[0] === 1 && isChoice[1] === 1 && isChoice[2]=== 1){
-      //   return KindValueArray
-      // }
-      //  else if (isChoice[0] === 1) {
-      //   return cityValueArray && etcRemove
-      //   // 품종만 선택시 
-      // } else {
-      //   return e && etcRemove
-      // }
+
     })
     // serDefaultData에 FilterAll 값을 한번 더 넣어주고
     serDefaultData(FilterAll)
@@ -261,115 +259,119 @@ function Info_Test() {
 
   return (
     <>
-    <div>
+      <div>
 
-    </div>
-    <div className="w-[1200px] mx-auto">
-    <p>도시:{optionCitySelect}</p>
-    <p>축종:{subOptionCitySelect}</p>
-    <p>품종:{optionKindSelect}</p>
-    <p>도시:{isChoice[0]}</p>
-    <p>축종:{isChoice[1]}</p>
-    <p>품종:{isChoice[2]}</p>
-
-      <div className="my-10 border-b-2 border-[#dac0a3]">
-        <div className="w-full h-full flex justify-between text-left ">
-          <div>
-            <p className='font-bold text-[#999]'>지역</p>
-            <select onChange={optionCity} className='text-xl   font-bold'>
-              <option value="모든 지역" >모든 지역</option>
-              {city.map((el, index) => <option key={index} value={el.orgdownNm}>{el.orgdownNm}</option>)}
-            </select>
-          </div>
-          {/* 동물 축종 */}
-          <div className="">
-            <p className='font-bold text-[#999]'>축종</p>
-{/* onChange={()=>{ChangeKind();subOption();}} */}
-{/*  onChange={() => {
-                  ChangeKind();
-                  subOption();
-                  }} */}
-            <select onChange={subOption}  
-            className='text-xl   font-bold' >
-              {kind.map((el, index) => <option key={index} value={el}>{el}</option>)}
-            </select>
-          </div>
-
-          {/* 동물 품종 */}
-          <div className="">
-            <p className='font-bold text-[#999]'>품종</p>
-            <select className='text-xl    font-bold w-[408px]'
-             onChange={optionKind} >
-               <option value="모든 품종">모든 품종</option>
-              {
-                //selected 가 모든 축종이면 모든 품종을 출력 : 아니라면 selecData가 참이라면 e.knm을 출력
-                {selectedSec}&& selectData && selectData.map((e, i) => {
-                  return (
-                    <option key={i} value={e.knm}>{e.knm}</option>
-                  )
-                })
-              }
-            </select>
-          </div>
-
-          <div className="flex items-center">
-
-            <button className="font-bold w-48 h-12  bg-[#dac0a3] whitespace-nowrap hover:text-white rounded-[20px]" onClick={searchResult}
-            >검색하기</button>
-
-          </div>
-
-        </div>
       </div>
+      <div className="max-w-[1200px] mx-auto">
+        
+        
+        <p>도시 : {cityCode}</p>
+        
+                {/* <p>도시:{optionCitySelect}</p>
+        <p>축종:{subOptionCitySelect}</p>
+        <p>품종:{optionKindSelect}</p>
+        <p>도시:{isChoice[0]}</p>
+        <p>축종:{isChoice[1]}</p>
+        <p>품종:{isChoice[2]}</p> */}
+    {/* <p>{kindCode}</p> */}
+        <div className="my-10 border-b-2 border-[#dac0a3]">
+          <div className="w-full h-full flex justify-between text-left ">
+            <div>
+              <p className='font-bold text-[#999]'>지역</p>
+              <select onChange={optionCity} className='text-xl font-bold'>
+                <option value="">모든 지역</option>
+                {city.map((el, index) => <option key={index} value={el.orgCd}>{el.orgdownNm}</option>)}
+              </select>
+            </div>
+            {/* 동물 축종 */}
+            <div className="">
+              <p className='font-bold text-[#999]'>축종</p>
+              {/* onChange={()=>{ChangeKind();subOption();}} */}
+              {/*  onChange={() => {
+                   ChangeKind();
+                   subOption();
+                   }} */}
+              <select onChange={subOption}
+                className='text-xl   font-bold'>
+                {kind.map((el, index) => <option key={index} value={index}>{el}</option>)}
+              </select>
+            </div>
 
-      <div className="flex text-[15px] gap-y-4 flex-wrap justify-start gap-x-3 ">
-        {
-          defaultData.length  === 0 ? <div className='w-full text-center'><p className='text-[32px] text-[#999] font-bold'>검색결과가 없습니다.</p></div> :
-            // 기본값을.map돌리고   serDefaulTdata에 Fiterall함수를 넣어 준다.
-            defaultData.map((e,i) => {
-       
-              return (
-                <div className="relative box-border border border-[#f1f1ef] flex-wrap basis-[32.5%] shadow-[4px_4px_4px_-4px_rgb(119, 112, 112)] rounded-[20px]" key={i}>
-                  <Link to={`/infodetail/${e.desertionNo}`} state={{ e: e }}>
-                    <div className='font-bold px-3 py-3 flex items-center justify-between'>
+            {/* 동물 품종 */}
+            <div className="">
+              <p className='font-bold text-[#999]'>품종</p>
+              <select className='text-xl    font-bold w-[408px]'
+                onChange={optionKind} value={optionKindSelect}>
+                <option value="모든 품종" >모든 품종</option>
+                {
+                  //selected 가 모든 축종이면 모든 품종을 출력 : 아니라면 selecData가 참이라면 e.knm을 출력
+                  { selectedSec } && selectData && selectData.map((e, i) => {
+                    return (
+                      <option key={i} value={e.knm}>{e.knm}</option>
+                    )
+                  })
+                }
+              </select>
+            </div>
 
-                      <p><FontAwesomeIcon icon={e.sexCd === 'M' ? faMars : e.sexCd === 'F' ? faVenus : ""} className='w-[18px] h-[18px] pr-1 align-text-bottom' />{e.sexCd === 'M' ? "남" : e.sexCd === 'F' ? "여" : "미상"} </p>
+            <div className="flex items-center">
+
+              <button className="font-bold w-48 h-12  bg-[#dac0a3] whitespace-nowrap hover:text-white rounded-[20px]" onClick={searchResult}
+              >검색하기</button>
+
+            </div>
+
+          </div>
+        </div>
+
+        <div className="flex text-[15px] gap-y-4 flex-wrap justify-start gap-x-3 ">
+          {
+            defaultData.length === 0 ? <div className='w-full text-center'><p className='text-[32px] text-[#999] font-bold'>검색결과가 없습니다.</p></div> :
+              // 기본값을.map돌리고   serDefaulTdata에 Fiterall함수를 넣어 준다.
+              defaultData.map((e, i) => {
+
+                return (
+                  <div className="relative box-border border border-[#f1f1ef] flex-wrap basis-[32.5%] shadow-[4px_4px_4px_-4px_rgb(119, 112, 112)] rounded-[20px]" key={i}>
+                    <Link to={`/infodetail/${e.desertionNo}`} state={{ e: e }}>
+                      <div className='font-bold px-3 py-3 flex items-center justify-between'>
+
+                        <p><FontAwesomeIcon icon={e.sexCd === 'M' ? faMars : e.sexCd === 'F' ? faVenus : ""} className='w-[18px] h-[18px] pr-1 align-text-bottom' />{e.sexCd === 'M' ? "남" : e.sexCd === 'F' ? "여" : "미상"} </p>
+                      </div>
+                      <div className="h-[350px] flex ">
+                        <img src={e.popfile} alt="img" className='w-full' />
+                      </div>
+
+                    </Link>
+                    <div className="pl-[10px] py-[10px]">
+                      <p className='font-bold text-[14px]'><span className='text-[#999]'>품종 :</span> {e.kindCd}</p>
+                      <p className='font-bold text-[14px]'><span className='text-[#999]'>나이 :</span> {e.age} 추정</p>
+                      <p className='font-bold text-[14px]'><span className='text-[#999]'>지역 :</span> {e.orgNm}</p>
                     </div>
-                    <div className="h-[350px] flex ">
-                      <img src={e.popfile} alt="img" className='w-full' />
-                    </div>
-
-                  </Link>
-                  <div className="pl-[10px] py-[10px]">
-                    <p className='font-bold text-[14px]'><span className='text-[#999]'>품종 :</span> {e.kindCd}</p>
-                    <p className='font-bold text-[14px]'><span className='text-[#999]'>나이 :</span> {e.age} 추정</p>
-                    <p className='font-bold text-[14px]'><span className='text-[#999]'>지역 :</span> {e.orgNm}</p>
                   </div>
-                </div>
-              )
-            })
-        }
-      </div>
-      {/* 페이지 네비 */}
-      <div className="w-[1200px] my-0 mx-auto justify-center">
-        <div className="mx-auto my-8 text-center ">
+                )
+              })
+          }
+        </div>
+        {/* 페이지 네비 */}
+        <div className="w-[1200px] my-0 mx-auto justify-center">
+          <div className="mx-auto my-8 text-center ">
 
-          <ul className='flex justify-center items-center list-style-none '>
+            <ul className='flex justify-center items-center list-style-none '>
 
-            <li
-              className='cursor-pointer w-[50px] h-[50px] rounded-full leading-10 text-cente relative block py-1.5 px-1.5 border-1 border-[#DAC0A3] mx-5
-          text-black shadow-sm' onClick={PrevBlock}>이전</li>
-            {PageList}
-            <li
-              className='cursor-pointer w-[50px] h-[50px] rounded-full leading-10 text-cente relative block py-1.5 px-1.5 mx-5 border-1 border-[#DAC0A3]
-        text-black shadow-sm focus:shadow-sm'  onClick={NextBlock}>다음</li>
-          </ul>
+              <li
+                className='cursor-pointer w-[50px] h-[50px] rounded-full leading-10 text-cente relative block py-1.5 px-1.5 border-1 border-[#DAC0A3] mx-5
+           text-black shadow-sm' onClick={PrevBlock}>이전</li>
+              {PageList}
+              <li
+                className='cursor-pointer w-[50px] h-[50px] rounded-full leading-10 text-cente relative block py-1.5 px-1.5 mx-5 border-1 border-[#DAC0A3]
+         text-black shadow-sm focus:shadow-sm'  onClick={NextBlock}>다음</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
 
 
-  </>
+    </>
   )
 }
 
