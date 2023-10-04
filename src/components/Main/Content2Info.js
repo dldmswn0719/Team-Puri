55a3a9bc783d2fcb382568999e8e68c8b16872a9
이지연 <[peaceprimavera@gmail.com]>
2023-10-05 00:08:04 +0900

    mainpage 영문

--- src/components/Main/Content2Info.js
+++ src/components/Main/Content2Info.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import enMessages from './../../locales/en.json';
+import krMessages from './../../locales/kr.json';
 
 function Content2Info() {
+
+    const language = useSelector(state => state.language);
+    const messages = language === 'en' ? enMessages : krMessages;
+
     return (
         <>
             <div className='md:w-[768px] lg:w-[1200px] m-auto flex items-center
             flex-col md:flex-row md:justify-between lg:flex-row lg:justify-between
             pt-20 md:pb-4 lg:py-[140px]'>
                 <div className='text-center tracking-tight lg:mb-0 mb-5'>
-                    <p className='text-2xl lg:text-[40px] font-bold leading-none'>삶에 대한 가치를 느낄 수 있는,<br />휴식과 즐거움이 만나는 곳</p>
-                    <p className='text-lg lg:text-2xl mt-[20px]'>동물권행동 푸리푸리는<br />100% 시민의 후원으로 운영되는<br />비영리단체로서 투명하고 정직하게 활동합니다.</p>
+                    <p className='text-2xl lg:text-[40px] font-bold leading-none'>{messages.cont2info1}<br />{messages.cont2info2}</p>
+                    <p className='text-lg lg:text-2xl mt-[20px]'>{messages.cont2info3}<br />{messages.cont2info4}<br />{messages.cont2info5}</p>
                 </div>
                 <div className='relative'>
                     <img src="./../../Images/Main/deco_needle_leaf.png" alt="2" className=' absolute w-[10%]
