5c47e5965d982472360cc45007dd9f179e8aad43
오창욱 <o2556@naver.com>
2023-10-05 18:33:38 +0900

    반응형 끝

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts}"],
+   content: ["./src/**/*.{js,ts}"],
   darkMode : "class",
   theme: {
+    screens : {
+      'fold' : {'max' : '350px'},
+      'sm': {'min': '351px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px'}
+    },
     extend: {},
   },
   plugins: [],
