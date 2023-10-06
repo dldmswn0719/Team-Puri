77e331026e062254504ec2bc8cd020717bf38bea
박은성 <cro3179@naver.com>
2023-10-06 18:51:02 +0900

    반응형 수정

--- src/components/Info/ScrollUpDown.js
+++ src/components/Info/ScrollUpDown.js
@@ -38,7 +38,7 @@ function ScrollUpDown() {
 
   return (
     <>
-      <div className='fixed rounded-full cursor-pointer min-w-[50px] min-h-[50px] sm:w-[40px] sm:h-[40px] leading-10 text-center  block py-1 px-1.5 lg:mx-3 border-1  text-white border-[#86bcd5] dark:border-1 dark:border-[#dadbdb] bg-[#86bcd5] dark:bg-[#404343] dark:text-[#ebf4f1] lg:right-6 bottom-6 md:right-2 md:bottom-[30px] sm:right-2 sm:bottom-[100px] opacity-70 z-10' onScroll={ScrollUpDown} onClick={click}>
+      <div className='fixed rounded-full cursor-pointer min-w-[50px] min-h-[50px] sm:w-[40px] sm:h-[40px] fold:w-[31px] fold:h-[31px] leading-10 text-center  block py-1 px-1.5 lg:mx-3 border-1  text-white border-[#86bcd5] dark:border-1 dark:border-[#dadbdb] bg-[#86bcd5] dark:bg-[#404343] dark:text-[#ebf4f1] lg:right-6 bottom-6 md:right-2 md:bottom-[30px] sm:right-2 sm:bottom-[100px] fold:right-0 fold:bottom-[70px] opacity-70 z-10' onScroll={ScrollUpDown} onClick={click}>
         {
           position <= 80 ? <FontAwesomeIcon icon={faArrowDown} /> : <FontAwesomeIcon icon={faArrowUp} />
         }
