547c3ec632d7ae3f4c0e832fca9f784c54071be8
박은성 <cro3179@naver.com>
2023-10-06 16:15:55 +0900

    반응형 수정 영어

--- src/components/Info/AnimalDetails.js
+++ src/components/Info/AnimalDetails.js
@@ -4,9 +4,6 @@ import TellButton from './TellButton'
 import { faLocationDot, faPaw } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
-
-
 function AnimalDetails() {
     const [mapX, setMapX] = useState(0)
     const [mapY, setMapY] = useState(0)
@@ -70,17 +67,17 @@ function AnimalDetails() {
     return (
         <>
             <div className="max-w-full dark:bg-[#272929]">
-                <div className=" max-w-[1200px] py-10 mx-auto ">
+                <div className=" max-w-[1200px] py-5 mx-auto ">
                     <div className="flex justify-center ">
                         <div className="w-full flex items-center justify-center flex-col mt-3">
                             <div className=''>
-                            <img className='w-[500px] h-[500px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px]  ' src={popfile} alt="img" />
+                            <img className='w-[500px] h-[500px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] fold:w-[250px] fold:h-[250px]  ' src={popfile} alt="img" />
                             </div>
-                            <div className="w-[500px]  md:w-[400px]  sm:w-[300px]  dark:text-[#ebf4f1]">
+                            <div className="w-[500px]  md:w-[400px]  sm:w-[300px] fold:w-[250px]  dark:text-[#ebf4f1]">
                                 <div className="">
-                                    <p className='text-xl font-bold mt-10 mb-2'><FontAwesomeIcon icon={faPaw} /> 상세 정보</p>
+                                    <p className='text-xl fold:text-lg font-bold mt-10 mb-2'><FontAwesomeIcon icon={faPaw} /> 상세 정보</p>
                                     <div className='border-b-2 border-[#86bcd5] dark:border-[#dadbdb]'></div>
-                                    <div className="dark:text-[#ebf4f1]">
+                                    <div className="dark:text-[#ebf4f1] text-sm">
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>품종 : <span className='text-black dark:text-[#ebf4f1]'>{kindCd}</span> </p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>성별 : <span className='text-black dark:text-[#ebf4f1]'>{sexCd === 'M' ? "남" : sexCd === 'F' ? "여" : "미상"}</span></p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>나이 : <span className='text-black dark:text-[#ebf4f1]'>{age}추정</span></p>
@@ -91,23 +88,23 @@ function AnimalDetails() {
                                     </div>
                                 </div>
                                 <div>
-                                    <p className='text-xl font-bold mt-10 mb-2'><FontAwesomeIcon icon={faPaw} />  구조 정보</p>
+                                    <p className='text-xl fold:text-lg font-bold mt-10 mb-2'><FontAwesomeIcon icon={faPaw} />  구조 정보</p>
                                     <div className='border-b-2 border-[#86bcd5] dark:border-[#dadbdb]'></div>
-                                    <div className="mt-3">
+                                    <div className="mt-3 fold:text-sm">
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>공고번호 : <span className='text-black dark:text-[#ebf4f1]'>{noticeNo}</span></p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>발견장소 : <span className='text-black dark:text-[#ebf4f1]'>{happenPlace}</span></p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>보호센터 : <span className='text-black dark:text-[#ebf4f1]'>{careNm}</span></p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1]'>담당부서 : <span className='text-black dark:text-[#ebf4f1]'>{orgNm}</span></p>
                                         <p className='mt-1 text-[#999] dark:text-[#ebf4f1] hidden lg:block md:hidden sm:hidden'>보호센터 Tel : <span className='text-black dark:text-[#ebf4f1]'>{careTel}</span></p>
                                         <p className='mt-1 text-[#999] hidden lg:block md:hidden sm:hidden dark:text-[#ebf4f1]'>담당부서 Tel : <span className='text-black dark:text-[#ebf4f1]'>{officetel}</span></p>
-                                        <p className='hidden lg:block md:hidden sm:hidden sm:text-base'><span className='text-black '></span>전화 문의는 평일 <span className='font-bold'>오전 9시 부터 오후 6시까지</span>입니다.</p>
-                                        <p className='hidden lg:block md:hidden sm:hidden'> {`(공휴일 제외)`}</p>
+                                        <p className='hidden lg:block md:hidden sm:hidden sm:text-base text-[#999]'>전화 문의는 평일 <span className='font-bold text-black '>오전 9시 <span className='text-[#999] '>부터</span><span className='text-black'>오후 6시</span></span>까지 입니다.</p>
+                                        <p className='hidden lg:block md:hidden sm:hidden text-black'> {`(공휴일 제외)`}</p>
                                         <TellButton />
                                         <p className='text-xl mt-11 dark:text-[#ebf4f1]'><FontAwesomeIcon icon={faLocationDot} color='#ff5b5b' />  보호소 위치</p>
                                     </div>
                                 </div>
                             </div>
-                            <div id='map' className='w-[500px] h-[500px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px]  mt-5'></div>
+                            <div id='map' className='w-[500px] h-[500px] md:w-[400px] md:h-[400px] sm:w-[300px] sm:h-[300px] fold:w-[250px] fold:h-[250px]   mt-5'></div>
                         </div>
                     </div>
                 </div>
