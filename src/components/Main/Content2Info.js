7da7fc30327c158c997829f76e633a5a379c8bcd
이지연 <peaceprimavera@gmail.com>
2023-10-06 16:15:32 +0900

    오류수정

--- src/components/Main/Content2Info.js
+++ src/components/Main/Content2Info.js
@@ -14,7 +14,7 @@ function Content2Info() {
         <div className='bg-white dark:bg-[#292929]'>
             <div className='md:w-[768px] lg:w-[1200px] m-auto flex items-center
             flex-col md:flex-row md:justify-between lg:flex-row lg:justify-between
-            pt-20 md:pb-5 md:pt-36 lg:py-[140px] relative'>
+            pt-20 md:pb-5 md:pt-36 lg:py-64 relative'>
                 <div className='text-center tracking-tight lg:mb-0 mb-5'>
                     <p className='text-2xl lg:text-[40px] font-bold leading-none dark:text-white'>{messages.cont2info1}<br />{messages.cont2info2}</p>
                     <p className='text-lg lg:text-2xl mt-[20px] dark:text-white'>{messages.cont2info3}<br />{messages.cont2info4}<br />{messages.cont2info5}</p>
--- src/components/Main/Content4Grid.js
+++ src/components/Main/Content4Grid.js
@@ -1,7 +1,49 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Content4Grid() {
+    const arr = [
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 암컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        },
+        {
+            "type" : "품종 : 개",
+            "gender" : "성별 : 수컷",
+            "age" : "2019년생 추정"
+        }
+    ];
     return (
         <div className='md:w-[768px] lg:w-[1200px] m-auto
         pt-28
@@ -10,16 +52,25 @@ function Content4Grid() {
                 {
                     Array(8).fill().map((_, i) => {
                         return (
-                            <li className='lg:w-[285px] w-52 m-auto
-                            cursor-pointer hover:scale-105 duration-300'><NavLink><img src={`./../../Images/Main/list${i + 1}.png`} alt={i + 1} /></NavLink>
+                            <li key={i} className={`lg:w-[285px] w-52 m-auto
+                            cursor-pointer duration-300 relative
+                            hover:scale-105 hover:after:w-full hover:after:h-full hover:after:bg-black hover:after:bg-opacity-30 hover:after:absolute hover:after:top-0 hover:after:left-0 hover:after:content-["개"] hover:after:text-white hover:after:text-center hover:after:items-center hover:after:text-3xl`}><Link to='/infodetail/444450202101068' /><img src={`./../../Images/Main/list${i + 1}.png`} alt={i + 1} />
                             </li>
                         )
                     })
                 }
+                {
+                    arr.map((e, index) => {
+                        console.log(e, index);
+                        
+                    })
+                }
             </ul>
-            <button className='bg-[#E75A56] text-white font-bold cursor-pointer duration-500 hover:bg-[#db3b36] rounded-full w-60 h-10 mt-10
-            md:w-80 md:h-16 md:text-xl md:mt-36
-            lg:w-80 lg:h-20 lg:text-2xl lg:mt-20'><NavLink to='/info' className='text-white'>보호동물 더 보기</NavLink></button>
+            <div className='text-center'>
+                <button className='bg-[#E75A56] text-white font-bold cursor-pointer duration-500 hover:bg-[#b3312c] rounded-full w-48 h-12 mt-10 text-lg
+                md:w-80 md:h-16 md:text-xl md:mt-36
+                lg:w-80 lg:h-20 lg:text-2xl lg:mt-20'><NavLink to='/info' className='text-white'>보호동물 더 보기</NavLink></button>
+            </div>
         </div>
     );
 }
