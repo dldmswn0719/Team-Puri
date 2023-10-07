531892bb8e5ed629c5cf67d6a71956793266dfa4
정서진 <94szerncse@gmail.com>
2023-10-07 15:51:58 +0900

    20231007

--- src/components/Review/Reviews.js
+++ src/components/Review/Reviews.js
@@ -108,9 +108,6 @@ function Reviews() {
                         color={likes[i] ? '#FAE5E9' : '#ff5b5b'}
                          className='w-[18px] h-[18px] hover:brightness-75 cursor-pointer' onClick={() => { document.querySelector("html")
                          toggleLike(i)}} />
-                    
-
-
 
                         <img className='w-6 h-5 mx-[10px] '
                         id="loginAlert" onClick={() => alert('개발진행중입니다.')} 
@@ -157,7 +154,7 @@ function Reviews() {
             <div className="mx-auto w-[160px]">
               <button onClick={() => { setCurrentPage(currentPage + 1) }} className='bg-[#8DBCD6] text-white w-[160px] h-[50px] my-[40px] '>더보기 +</button> 
               
-              <button className='bg-[#8DBCD6] text-white w-[160px] h-[50px] my-[5px] '>무한스크롤</button>
+              {/* <button className='bg-[#8DBCD6] text-white w-[160px] h-[50px] my-[5px] '>무한스크롤</button> */}
             </div>
           </div>
         </div>
@@ -169,12 +166,15 @@ function Reviews() {
       {
         modalOpen &&
         <div>
-          <div className="opacity-60 fixed left-2/4 lg:top-2/4 md:top-1/2 w-full h-full bg-[#e6e6e6] p-[20%] -translate-x-2/4 z-[60] -translate-y-2/4  sm:top-1/2 "></div>
+          <div onClick={() => { setModalOpen(false); document.querySelector("html").classList.remove("fixed") }}
+           className="opacity-60 fixed left-2/4 lg:top-2/4 md:top-1/2 w-full h-full bg-[#e6e6e6] p-[20%] -translate-x-2/4 z-[60] -translate-y-2/4  sm:top-1/2 "></div>
           <div className="relative">
             <div className="fixed z-[70] left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 lg:w-[900px] w-11/12 p-5 lg:h-[500px] md:h-[500px] h-[600px] bg-white rounded-[20px] md:w-5/6">
 
 
-              <FontAwesomeIcon onClick={() => { setModalOpen(false); document.querySelector("html").classList.remove("fixed") }} icon={faXmark} className='hover:text-[pink] absolute w-[30px] h-[30px] right-[1%] top-[2%] z-40' />
+              <FontAwesomeIcon onClick={() => { setModalOpen(false); document.querySelector("html").classList.remove("fixed") }} icon={faXmark} className='hover:text-[pink] absolute w-[30px] h-[30px] right-[1%] top-[2%] z-40 sm:hidden' />
+
+
 
               <div className="lg:flex md:flex  w-full lg:h-full md:h-full h-48  relative">
                 <div style={{ backgroundImage: `url(${Mainlist[one].img})`, backgroundSize: "cover", backgroundPosition: "center", flexBasis: "55%", height: "100%", margin: 0 }} className='rounded-tl-[20px] rounded-l-[20px]'></div>
@@ -214,7 +214,7 @@ function Reviews() {
                         <li><img className='w-[20px] h-[20px] mt-1' src="./../Images/Review/4.png" alt="airplane" /></li>
                       </ul>
                       <ul>
-                        <li><img className='w-[20px] h-[20px]' src="./../Images/Review/mark.svg" alt="airplane" /></li>
+                        <li><img className='w-[20px] h-[20px]' src="./../Images/Review/mark.svg" alt="mark" /></li>
                       </ul>
                     </div>
                     <div>
