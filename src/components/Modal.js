cd9f79b8887c27b4c25b84968808a5925f5329c1
이지연 <[peaceprimavera@gmail.com]>
2023-10-08 18:23:10 +0900

    .

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,7 @@ function Modal({error, onClose}) {
     return (
         <div className='fixed top-0 left-0 w-full h-full bg-black/70 z-50 flex justify-center items-center dark:bg-black/80'>
             <div className='basis-[360px] bg-white pt-14 px-5 pb-10 rounded-lg flex justify-center flex-wrap dark:bg-[#272929]'>
-                <FontAwesomeIcon icon={faTriangleExclamation} className='basis-full text-7xl text-red-500' />
+                <div><img src='./../Images/Main/alertdog.png' alt='alert' className='w-1/2 mx-auto' /></div>
                 <p className='text-base font-bold my-6 mx-0 dark:text-[#ebf4f1]'>{error}</p>
                 <button onClick={onClose} className='w-full p-[10px] rounded-md bg-[#60a7c8] border-none text-white cursor-pointer hover:bg-[#327290] dark:bg-[#404343]'>확인</button>
             </div>
